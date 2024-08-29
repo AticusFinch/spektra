21eadfb198481cfcff2641f18cd99d30d1eeca23
Stasa Bastrica <stasabastrica7@gmail.com>
2024-08-29 10:37:19 +0200

    hero

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import "./styles/globals.css";
 import { ApolloProvider } from "@apollo/client/react";
+import { Roboto } from "@next/font/google";
 import { client } from "../lib/apollo";
 import Head from "next/head";
 import { useRouter } from "next/router";
@@ -8,6 +9,11 @@ export const metadata = {
   title: "Asocijacija Spektra",
   description: "Veb-sajt Asocijacije Spektra",
 };
+
+const roboto = Roboto({
+  weight: ["300", "400", "700", "900"],
+  subsets: ["latin"],
+});
 
 export default function MyApp({ Component, pageProps }) {
   const router = useRouter();
@@ -22,7 +28,9 @@ export default function MyApp({ Component, pageProps }) {
         <link rel="icon" href="ico.ico" />
       </Head>
       <ApolloProvider client={client}>
-        <Component {...pageProps} />
+        <div className={roboto.className}>
+          <Component {...pageProps} />
+        </div>
       </ApolloProvider>
     </>
   );
