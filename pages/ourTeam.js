80e02a0b74ee29ee49dc91874440f6b4214b0345
Stasa Bastrica <stasabastrica7@gmail.com>
2025-04-11 14:41:53 +0200

    Commit

--- pages/ourTeam.js
+++ pages/ourTeam.js
@@ -106,20 +106,20 @@ const teamMembers = [
   },
 ];
 
+function shuffleArray(array) {
+  const newArray = [...array]; // Create a copy of the array
+  for (let i = newArray.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [newArray[i], newArray[j]] = [newArray[j], newArray[i]];
+  }
+  return newArray;
+}
+
+const shuffledTeamMembers = shuffleArray(teamMembers);
+
 const OurTeam = () => {
   const router = useRouter();
   const { locale } = router;
-
-  function shuffleArray(array) {
-    const newArray = [...array]; // Create a copy of the array
-    for (let i = newArray.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [newArray[i], newArray[j]] = [newArray[j], newArray[i]];
-    }
-    return newArray;
-  }
-
-  const shuffledTeamMembers = shuffleArray(teamMembers);
 
   return (
     <>
